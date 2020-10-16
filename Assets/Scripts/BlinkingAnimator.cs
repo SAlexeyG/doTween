ce3f6a8d32376b0fd363193e68639c2a8cbd6280
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-16 13:25:07 +0300

    2

---
+++ Assets/Scripts/BlinkingAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using DG.Tweening;
+
+public class BlinkingAnimator : MonoBehaviour
+{
+    [SerializeField] private GameObject circle; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(0f, 0.1f));
+        sequence.Append(transform.DOScale(1f, 0.1f));
+        sequence.Append(transform.DOMove(Vector3.right, 0.1f));
+    }
+}
---
+++ Assets/Scripts/CircleHalfAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using DG.Tweening;
+
+public class CircleHalfAnimator : MonoBehaviour
+{
+    [SerializeField] private GameObject circle1;
+    [SerializeField] private GameObject circle2;
+
+    private void Update()
+    {
+        circle1.transform.DORotate(circle1.transform.eulerAngles +
+            new Vector3(0, 0, 180), 1f);
+        circle2.transform.DORotate(circle2.transform.eulerAngles +
+            new Vector3(0, 0, -180), 1f);
+    }
+}
