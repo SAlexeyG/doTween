c729af760c99ff4d88b01f2dd7b92a1e1af21f0b
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-16 12:23:16 +0300

    1

---
+++ Assets/Scripts/ButtonAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+
+public class ButtonAnimator : MonoBehaviour
+{
+    public void Shrink()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(0.7f, 0.1f));
+        sequence.Append(transform.DOScale(1f, 0.1f));
+    }
+}
